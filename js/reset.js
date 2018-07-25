52aef34a1b77ea6bc5552c7e4e58bf11f61a2625
zzz111111 <937741304@qq.com>
2018-07-25 23:52:17 +0800

    reset.js2

--- js/reset.js
+++ js/reset.js
@@ -1,3 +1,4 @@
+/*
 var doc = document,
     win = window,
     oBody = doc.documentElement || doc.body,
@@ -5,7 +6,18 @@ var doc = document,
 rem();
 addEventListener(resize, rem, false);
 function rem() {
-    /*doc.body.clientWidth是获取当前的body的宽度，640是我们的移动端的最大宽度，相除就会得出一个比例，在乘以100px，就会得出我们当前的1rem等于多少px*/
+    //doc.body.clientWidth是获取当前的body的宽度，640是我们的移动端的最大宽度，相除就会得出一个比例，在乘以100px，就会得出我们当前的1rem等于多少px
     oBody.style.fontSize = 100 * (doc.body.clientWidth / 640) + "px";
     console.log(oBody.style.fontSize);
 }
+*/
+
+var html = document.querySelector('html');
+	 rem();
+	window.addEventListener('resize',function () {
+		rem();
+	})
+	function rem() {
+		var width = html.getBoundingClientRect().width;
+		html.style.fontSize = width/10+ 'px';
+	}
